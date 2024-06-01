ab93ed9fc21323d78cc7f60d5e7f9a9767c3236d
Emerson <emerrueda215@gmail.com>
2024-06-01 14:54:51 -0500

    Mi aplicación Django Girls, primer commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+girls = ["Rachel", "Majo","Sofia", "Margarita", "Fernanda"]
+
+def hi(name):
+    print("Hola " + name +"!")
+
+for name in girls:
+    hi(name)
+    print("Siguiente")
+
+for i in range(1, 6):
+    print(i)
